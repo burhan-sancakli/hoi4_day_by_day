f66802f123ce4d681ea0c413b5a35cb79e3f0958
ulusfark01 <49228135+ulusfark01@users.noreply.github.com>
2021-02-25 12:33:10 +0300

    Add files via upload

---
+++ day_by_day_outdated/common/defines/time_defines.lua
@@ -0,0 +1,17 @@
+NDefines.NCountry.EVENT_PROCESS_OFFSET = 1 --Было 20	
+NDefines.NCountry.DAYS_OF_WAR_BEFORE_SURRENDER = 1 -- 7
+
+
+NDefines.NTechnology.BASE_RESEARCH_POINTS_SAVED = 1.25 --30.0
+NDefines.NTechnology.BASE_YEAR_AHEAD_PENALTY_FACTOR = 2 --2
+NDefines.NTechnology.BASE_TECH_COST = 3.54 --85
+
+NDefines.NFocus.FOCUS_POINT_DAYS = 0.3 --Было 10
+NDefines.NFocus.MAX_SAVED_FOCUS_PROGRESS = 0 --Было 10
+
+
+
+
+
+NDefines.NMilitary.LAND_SPEED_MODIFIER = 1.2 --0.05
+NDefines.NMilitary.STRATEGIC_SPEED_BASE = 240.0 --10.0
